7f7a2131fb6faa3003eb246b88242964f255980c
Felipe <camargo963@gmail.com>
2025-10-05 16:03:22 -0300

    feat: Post migration and model

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
+
+Route::resource('posts', PostController::class);
