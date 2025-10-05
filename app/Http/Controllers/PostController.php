c28d276671af8b4134f4938452f4500b6d0870f8
Felipe <camargo963@gmail.com>
2025-10-05 16:52:51 -0300

    feat: Post Index

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class PostController extends Controller
 {
-    //
+    public function index()
+    {
+        return Inertia::render(
+            'Post/Index',
+            [
+                'posts' => Post::all()
+            ]
+        );
+    }
 }
---
+++ resources/js/pages/Post/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1>Meu CRUD de inércia</h1>
+        <table>
+            <thead>
+                <tr>
+                    <th v-for="header in headers" :key="header">
+                        {{ header }}
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post.id">
+                    <td>{{ post.title }}</td>
+                    <td>{{ post.body }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { Post } from '@/types/Post';
+
+defineProps<{
+    posts: Post[]
+}>();
+
+const headers = ['posts', 'body'];
+</script>
